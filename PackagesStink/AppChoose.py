318a4c20eba737406847ed7a7d3a9e612b503143
Rl242Dev <74558778+Rl242Dev@users.noreply.github.com>
2021-07-23 15:36:05 +0200

    Same as Main.py

--- PackagesStink/AppChoose.py
+++ PackagesStink/AppChoose.py
@@ -1,7 +1,17 @@
 import sys
 import time
 
-myList = ['NoRecoil','ShortCuts','PasswordGenerator']
+class myDirOs:
+    def my_function():
+        import os
+        os.startfile(dir)
+        os.system(dir)
+
+        import subprocess
+        subprocess.Popen([dir])
+        subprocess.call(dir)
+
+myList = ['NoRecoil','ShortCuts','PasswordGenerator','Calculator']
 
 print('The version that you are currently using is Version 0.3')
 AppChoose = input('Hey Welcome to AppChoose, if you want the list of Application type \"List\", or just type the name or the application/file. ')
@@ -11,57 +21,42 @@ if AppChoose == ('List'):
     AppList = input('Which app/file do you want to execute.')
     if AppList == ('NoRecoil'):
         dir = 'C:\\Users\\Robin\\Desktop\\GithubStink\\Stink\\PackagesStink\\Dir\\NoRecoil.ahk'
-        import os
-        os.startfile(dir)
-        os.system(dir)
-
-        import subprocess
-        subprocess.Popen([dir])
-        subprocess.call(dir)
+        myDirOs
         time.sleep(3)
         print('The NoRecoil Started, you can close this Window.')
     if AppList == ('ShortCuts'):
         dir = 'C:\\Users\\Robin\\Desktop\\GithubStink\\Stink\\PackagesStink\\Dir\\ShortCuts.py'
-        import os
-        os.startfile(dir)
-        os.system(dir)
-
-        import subprocess
-        subprocess.Popen([dir])
-        subprocess.call(dir)
+        myDirOs
         time.sleep(3)
         sys.exit
-if AppList == ('PasswordGenerator'):
+    if AppList == ('PasswordGenerator'):
      dir = 'C:\\Users\\Robin\\Desktop\\GithubStink\\Stink\\PackagesStink\\Dir\\PasswordGenerator.py'
-     import os
-     os.startfile(dir)
-     os.system(dir)
-
-     import subprocess
-     subprocess.Popen([dir])
-     subprocess.call(dir)
+     myDirOs
+    if AppList == ('Calculator'):
+     dir = 'C:\\Users\\Robin\\Desktop\\GithubStink\\Stink\\PackagesStink\\Dir\\Calculator.py'
+     myDirOs
+     time.sleep(3)
+     sys.exit
 if AppChoose == ('ShortCuts'):
     dir = 'C:\\Users\\Robin\\Desktop\\GithubStink\\Stink\\PackagesStink\\Dir\\ShortCuts.py'
-    import os
-    os.startfile(dir)
-    os.system(dir)
-
-    import subprocess
-    subprocess.Popen([dir])
-    subprocess.call(dir)
+    myDirOs
     time.sleep(3)
     sys.exit
 if AppChoose == ('NoRecoil'):
         dir = 'C:\\Users\\Robin\\Desktop\\GithubStink\\Stink\\PackagesStink\\Dir\\NoRecoil.ahk'
-        import os
-        os.startfile(dir)
-        os.system(dir)
-
-        import subprocess
-        subprocess.Popen([dir])
-        subprocess.call(dir)
+        myDirOs
         time.sleep(3)
         print('The NoRecoil Started, you can close this Window.')
+if AppChoose == ('Calculator'):
+     dir = 'C:\\Users\\Robin\\Desktop\\GithubStink\\Stink\\PackagesStink\\Dir\\Calculator.py'
+     myDirOs
+     time.sleep(3)
+     sys.exit
+if AppChoose == ('PasswordGenerator'):
+    dir = 'C:\\Users\\Robin\\Desktop\\GithubStink\\Stink\\PackagesStink\\Dir\\PasswordGenerator.py'
+    myDirOs
+    time.sleep(3)
+    sys.exit
 else:
     time.sleep(3)
     print('This isn\'t an file or Application avalable on Stink')
