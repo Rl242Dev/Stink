e503f51fdeb33a25db0893e20888ad797d3a0a9a
Rl242Dev <74558778+Rl242Dev@users.noreply.github.com>
2021-07-23 15:35:26 +0200

    Updated To clean Execution of a file

--- PackagesStink/Main.py
+++ PackagesStink/Main.py
@@ -1,9 +1,17 @@
 import  sys
 import  time
 
-myHelp = ["To run an app type the name of the app/file, if there's any error(s) you can message me on Github Name:Rl242Dev (Same for suggestions just message me). You gotta type the exact name of the file/app to execute it."]
-myList = ["NoRecoil"]
+class myDirOs:
+    def my_function():
+        import os
+        os.startfile(dir)
+        os.system(dir)
 
+        import subprocess
+        subprocess.Popen([dir])
+        subprocess.call(dir)
+
+myHelp = ["To run an app type the name of the app/file, if there's any error(s) you can message me on Github Name:Rl242Dev (Same for suggestions just message me). You gotta type the exact name of the file/app to execute it."]
 
 print('The version that you are currently using is Version 0.3')
 time.sleep(5)
@@ -17,14 +25,9 @@ if TotalChoose == ('Help'):
 if TotalChoose == ('AppChoose'):
         print('This is gonna start another Window')
         time.sleep(3)
-        dir = 'C:\\Users\\Robin\\Desktop\\PackagesStink\\AppChoose.py'
-        import os
-        os.startfile(dir)
-        os.system(dir)
-
-        import subprocess
-        subprocess.Popen([dir])
-        subprocess.call(dir)
+        dir = 'C:\\Users\\Robin\\Desktop\\GithubStink\\Stink\\PackagesStink\\AppChoose.py'
+        myDirOs
+        time.sleep(3)
         sys.exit
 
 
