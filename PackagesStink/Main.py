e8541e851d08a1a2146f92e6fbda36efd7a84f01
Rl242Dev <74558778+Rl242Dev@users.noreply.github.com>
2021-07-26 22:32:49 +0200

    Up Version

--- PackagesStink/Main.py
+++ PackagesStink/Main.py
@@ -13,7 +13,7 @@ class myDirOs:
 
 myHelp = ["To run an app type the name of the app/file, if there's any error(s) you can message me on Github Name:Rl242Dev (Same for suggestions just message me). You gotta type the exact name of the file/app to execute it."]
 
-print('The version that you are currently using is Version 0.4')
+print('The version that you are currently using is Version 0.5')
 time.sleep(5)
 TotalChoose = input("Welcome to Stink, if you want help to see Commands type \"Help\'.To Choose an App type \"AppChoose\".") 
 if TotalChoose == ('Help'):
